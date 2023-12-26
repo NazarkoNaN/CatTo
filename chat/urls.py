cf7e0fc81141426b49a52ffac8410ce4757adefc
NazarkoNaN <nazar.ivanyuk@gmail.com>
2023-12-26 02:30:20 -0800

    works

--- chat/urls.py
+++ chat/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import MessageDetail, MessageList, MessageCreate, ChatList, chat_messages_list, ChatMessageList
+from .views import MessageDetail, MessageList, MessageCreate, ChatList, ChatMessageList
 
 
 urlpatterns = [
@@ -8,6 +8,5 @@ urlpatterns = [
     path('create/', MessageCreate.as_view(), name='message_create'),
 
     path('chats/', ChatList.as_view(), name='chat_list'),
-    #path('chats/<int:pk>/', chat_messages_list, name='chat_messages_lsit'),
     path('chats/<int:pk>/', ChatMessageList.as_view(), name='chat_messages_lsit'),
 ]
